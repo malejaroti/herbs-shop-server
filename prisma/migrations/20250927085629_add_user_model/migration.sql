5ee95ce403bd9f90688cb30dc6f2b6c7d1c32ab3
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-09-27 10:58:50 +0200

    feat: add Prisma seed script and migration files for User model with role enum

---
+++ prisma/migrations/20250927085629_add_user_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('user', 'admin');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'user',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
