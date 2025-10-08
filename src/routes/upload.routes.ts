bdfa851f5101824424e8e1a46785c2dfd238de83
Alejandra Rodriguez Tibana <30965660+malejaroti@users.noreply.github.com>
2025-10-08 20:11:25 +0200

    fix(upload.routes): correct typo in comment and add logging for Cloudinary response

--- src/routes/upload.routes.ts
+++ src/routes/upload.routes.ts
@@ -1,4 +1,4 @@
-import { type Request, type Response, type NextFunction, Router } from 'express';
+import { type Request, type Response, type NextFunction, Router, response } from 'express';
 import uploader from "../middleware/cloudinary.config";
 
 const router = Router()
@@ -8,7 +8,8 @@ router.post("/", uploader.single("image"), (req: Request, res: Response, next: N
   console.log("file is: ", req.file);
 
   if (!req.file) {
-    // this will happend if cloudinary rejects the image for any reason
+    // this will happen if cloudinary rejects the image for any reason
+    console.log("Response to Cloudinary upload: ", res)
     res.status(400).json({
       errorMessage: "There was a problem uploading the image. Check image format and size."
     })
@@ -17,7 +18,7 @@ router.post("/", uploader.single("image"), (req: Request, res: Response, next: N
 
   // get the URL of the uploaded file and send it as a response.
   // 'imageUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend (response.data.imageUrl)
-
+  console.log("Received URL from Cloudinary: ", req.file.path )
   res.json({ imageUrl: req.file.path });
 });
 
